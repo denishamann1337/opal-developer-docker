09a9167b7bfc60c25c18a4f004b5cf3949ddc082
Denis Hamann <40381127+denishamann1337@users.noreply.github.com>
2018-11-13 09:37:35 +0100

    Update runEnvironment.sh
    adds exec to start intellij after setup

--- runEnvironment.sh
+++ runEnvironment.sh
@@ -1,7 +1,8 @@
 docker build -t opaldev .
 IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + $IP
-docker run -ti --rm \
+docker run --name opal_intellij -ti --rm \
        -e DISPLAY=$IP:0 \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
-       opaldev
+       opaldev# run idea
+docker exec -u developer -d opal_intellij /usr/local/bin/idea.sh

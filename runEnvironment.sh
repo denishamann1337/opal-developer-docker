bdcbb630ce36e053a6b7dfdc101e6db855b8539c
Denis Hamann <40381127+denishamann1337@users.noreply.github.com>
2019-02-28 14:44:20 +0100

    Update runEnvironment.sh

--- runEnvironment.sh
+++ runEnvironment.sh
@@ -1,8 +1,4 @@
 docker build -t opaldev .
-IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
-xhost + $IP
 docker run --name opal_intellij -ti --rm \
-       -e DISPLAY=$IP:0 \
-       -v /tmp/.X11-unix:/tmp/.X11-unix \
-       opaldev# run idea
-docker exec -u developer -d opal_intellij /usr/local/bin/idea.sh
+              opaldev# run idea
+#docker exec -u developer -d opal_intellij /usr/local/bin/idea.sh
